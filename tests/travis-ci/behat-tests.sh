3a72478c0863a8eb918a43ec5ff004e78d906f99
Alex Finnarn <alexfinnarn@gmail.com>
2018-08-16 15:08:44 -0700

    debug where behat binary is located

--- tests/travis-ci/behat-tests.sh
+++ tests/travis-ci/behat-tests.sh
@@ -7,8 +7,8 @@ nc -zvv 127.0.0.1 8057; out=$?; while [[ $out -ne 0 ]]; do echo "Retry hit port 
 earlyexit
 
 echo "Running Express headless tests..."
-cd ${ROOT_DIR}/backdrop/modules/${MODULE_NAME}
-ls -al
+ls -al ${ROOT_DIR}/backdrop/modules
+
 
 ${ROOT_DIR}/backdrop/modules/${MODULE_NAME}/tests/behat/bin/behat --stop-on-failure --strict --config ${ROOT_DIR}/backdrop/modules/${MODULE_NAME}/tests/behat/behat.travis.yml --verbose --tags ${EXPRESS_HEADLESS_BEHAT_TAGS}
 earlyexit
