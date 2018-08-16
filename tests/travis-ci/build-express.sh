e2b40b576ee9c0920fed4a1f20a96a18e7085dbf
Alex Finnarn <alexfinnarn@gmail.com>
2018-08-16 15:43:11 -0700

    remember to change SQL user

--- tests/travis-ci/build-express.sh
+++ tests/travis-ci/build-express.sh
@@ -2,6 +2,6 @@
 
 # Install a site.
 cd ${ROOT_DIR}/backdrop
-$HOME/.composer/vendor/bin/drush si --db-url=mysql://newuser:password@127.0.0.1/backdrop -y
+$HOME/.composer/vendor/bin/drush si --db-url=mysql://backdrop:backdrop@127.0.0.1/backdrop -y
 
 exit 0
