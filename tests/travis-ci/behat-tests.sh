e7ce6ddab27904858744bddaa96cc8c4c1153345
Alex Finnarn <alexfinnarn@gmail.com>
2018-08-17 11:35:39 -0700

    try cloning contrib modules

--- tests/travis-ci/behat-tests.sh
+++ tests/travis-ci/behat-tests.sh
@@ -14,7 +14,7 @@ earlyexit
 
 # Enable any additional modules used during test runs.
 echo Enabling additional testings modules...
-$HOME/.composer/vendor/bin/drush dl ${ADD_CONTRIB_MODULES} -y
+git clone https://github.com/backdrop-contrib/${ADD_CONTRIB_MODULES}.git ${ROOT_DIR}/backdrop/modules/
 $HOME/.composer/vendor/bin/drush en ${ADD_CONTRIB_MODULES} -y
 
 $HOME/.composer/vendor/bin/drush en ${ADD_CUSTOM_MODULES} -y
