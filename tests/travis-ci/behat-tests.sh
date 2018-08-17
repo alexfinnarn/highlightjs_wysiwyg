51ff1e3c0a94039295440e54dfe03877df59d5d0
Alex Finnarn <alexfinnarn@gmail.com>
2018-08-17 11:38:18 -0700

    try cloning contrib modules

--- tests/travis-ci/behat-tests.sh
+++ tests/travis-ci/behat-tests.sh
@@ -6,12 +6,6 @@ $HOME/.composer/vendor/bin/drush backdrop-runserver 127.0.0.1:8057 > /dev/null 2
 nc -zvv 127.0.0.1 8057; out=$?; while [[ $out -ne 0 ]]; do echo "Retry hit port 8057..."; nc -zvv localhost 8057; out=$?; sleep 5; done
 earlyexit
 
-# Enable module.
-cd $ROOT_DIR/backdrop
-echo Enabling bundle module...
-$HOME/.composer/vendor/bin/drush en ${MODULE_NAME} -y
-earlyexit
-
 # Enable any additional modules used during test runs.
 echo Enabling additional testings modules...
 git clone https://github.com/backdrop-contrib/${ADD_CONTRIB_MODULES}.git ${ROOT_DIR}/backdrop/modules/
@@ -19,6 +13,12 @@ $HOME/.composer/vendor/bin/drush en ${ADD_CONTRIB_MODULES} -y
 
 $HOME/.composer/vendor/bin/drush en ${ADD_CUSTOM_MODULES} -y
 $HOME/.composer/vendor/bin/drush cc all
+earlyexit
+
+# Enable module.
+cd $ROOT_DIR/backdrop
+echo Enabling bundle module...
+$HOME/.composer/vendor/bin/drush en ${MODULE_NAME} -y
 earlyexit
 
 # Run any database updates.
